26dd8f6d34355c5b98a028d0757835e478e94075
Pratchaya0 <pratchayaworks@gmail.com>
2024-09-03 16:49:36 +0700

    fix bug

--- repositories/UserRepository/UserRepository.go
+++ repositories/UserRepository/UserRepository.go
@@ -9,7 +9,7 @@ type UserRepository struct {
 	db *gorm.DB
 }
 
-func NewGormUserRepository(db *gorm.DB) UserInterface {
+func NewGormUserRepository(db *gorm.DB) *UserRepository {
 	return &UserRepository{db: db}
 }
 
